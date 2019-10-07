14906bd76d41db24b58e753e04f1dc43ea28d5fc
 Ilia Goncharenko <dante@MacBook-Pro-Ilia.local>
2019-10-07 19:23:11 -0400

    Initial commit

---
+++ PartyMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyMaker
+//
+//  Created by  Ilia Goncharenko on 2019-10-07.
+//  Copyright © 2019 711Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
