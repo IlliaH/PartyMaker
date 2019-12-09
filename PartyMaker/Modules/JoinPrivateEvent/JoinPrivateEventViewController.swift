737e1b3c9ec56336045cd505b90ad596d613e349
Bohdan Bodak <noopps0@gmail.com>
2019-12-09 10:52:39 -0500

    Implemented navigation from JoinPrivateEvent to EventDetails

--- PartyMaker/Modules/JoinPrivateEvent/JoinPrivateEventViewController.swift
+++ PartyMaker/Modules/JoinPrivateEvent/JoinPrivateEventViewController.swift
@@ -13,11 +13,20 @@ class JoinPrivateEventViewController: UIViewController {
     @IBOutlet weak var secretCodeTextField: CustomHoshiTextField!
     
     let eventService = EventService()
+    var eventId: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "fromJoinToEventDetails" {
+            let navigationVC = segue.destination as! UINavigationController
+            let detailsVC = navigationVC.topViewController as! EventDetailsViewController
+            detailsVC.eventId = eventId
+        }
     }
     
     @IBAction func joinButtonOnTouch(_ sender: UIButton) {
@@ -29,6 +38,7 @@ class JoinPrivateEventViewController: UIViewController {
                         print(error as Any)
                     }
                     else if let event = event {
+                        self.eventId = event.id
                         self.performSegue(withIdentifier: "fromJoinToEventDetails", sender: nil)
                     }
                 }
