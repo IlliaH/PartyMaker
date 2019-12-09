9f751bdc0ef6d5e5d0176a767a704f47f10860a7
Ilia Goncharenko <danteoutoftime@gmail.com>
2019-12-09 18:45:07 -0500

    added UserEventsViewController

---
+++ PartyMaker/Modules/UserEventsScreen/UserEventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserEventsViewController.swift
+//  PartyMaker
+//
+//  Created by  Ilia Goncharenko on 2019-12-09.
+//  Copyright © 2019 711Development. All rights reserved.
+//
+
+import UIKit
+
+class UserEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
