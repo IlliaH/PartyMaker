379a89aa6dbb1671986a95cb90fc5cbfc5a4dd5e
Ilia Goncharenko <danteoutoftime@gmail.com>
2019-12-10 16:54:52 -0500

    added Alert class

---
+++ PartyMaker/Enities/Alert.swift
@@ -0,0 +1,19 @@
+//
+//  Alert.swift
+//  PartyMaker
+//
+//  Created by  Ilia Goncharenko on 2019-12-10.
+//  Copyright © 2019 711Development. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Alert {
+    static func createAlert(with title: String, with message: String) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(alertButton)
+        return alert
+    }
+}
